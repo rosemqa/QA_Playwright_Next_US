ed370c032728c8fec2668667f7b8c53c2f8a585d
rosemqa <ers6wobmo09m@mail.ru>
2025-12-16 21:33:56 +0300

    added tracing and videos

--- conftest.py
+++ conftest.py
@@ -1,4 +1,6 @@
 import time
+from datetime import datetime
+import allure
 import pytest
 from playwright.sync_api import sync_playwright, Browser, Page
 from pages.cart_page import CartPage
@@ -24,12 +26,18 @@ def page(browser: Browser) -> Page:
     context = browser.new_context(
         viewport={"width": 1920, "height": 1080},
         # no_viewport=True,
-        # record_video_dir='./videos'
+        record_video_dir='./videos'
     )
+    context.tracing.start(snapshots=True, screenshots=True, sources=True)
     page = context.new_page()
     page.set_default_timeout(timeout=10000)
     yield page
+    tracing_file = f'./tracing/{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.zip'
+    context.tracing.stop(path=tracing_file)
     context.close()
+
+    allure.attach.file(tracing_file, name='trace', extension='zip')
+    allure.attach.file(page.video.path(), name='video', attachment_type=allure.attachment_type.WEBM)
 
 
 @pytest.fixture()
